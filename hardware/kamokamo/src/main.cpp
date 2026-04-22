3de3aacf9fca2c4558824172021d97d19f50a33a
miki <chiarichan.29@gmail.com>
2026-04-23 00:27:35 +0900

    Add counter function to Atom Lite button

--- hardware/kamokamo/src/main.cpp
+++ hardware/kamokamo/src/main.cpp
@@ -1,11 +1,14 @@
 #include <M5Atom.h>
 
+int count = 0;
+bool lastButtonState = false;
+
 void setup() {
   M5.begin(true, false, true);
-  delay(50);
-
   Serial.begin(115200);
-  Serial.println("Atom Lite button test start");
+  delay(1000);
+
+  Serial.println("Karugamo Counter Start");
 
   // 待機中は青
   M5.dis.drawpix(0, 0x0000ff);
@@ -14,14 +17,24 @@ void setup() {
 void loop() {
   M5.update();
 
-  if (M5.Btn.wasPressed()) {
-    Serial.println("button pressed");
+  bool currentButtonState = M5.Btn.isPressed();
 
-    // 押したら緑
+  // 押された瞬間だけ1回カウントする
+  if (currentButtonState == true && lastButtonState == false) {
+    count++;
+
+    Serial.print("count: ");
+    Serial.println(count);
+
+    // 緑に光る
     M5.dis.drawpix(0, 0x00ff00);
     delay(200);
 
-    // 元に戻す
+    // 青に戻す
     M5.dis.drawpix(0, 0x0000ff);
   }
+
+  lastButtonState = currentButtonState;
+
+  delay(10);
 }
